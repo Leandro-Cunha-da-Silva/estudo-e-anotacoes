1bc7fb1024a3669ebbd45c326f97cf5111ca09c6
Leandro Silva <leandro10cunhadasilva@gmail.com>
2026-05-11 16:59:08 -0300

    JS básico finalizado

---
+++ src/03-js/01-basico/20-exercicio-02/src/js/index.js
@@ -0,0 +1,7 @@
+// const boletoPago = false;
+
+// if(boletoPago){
+//     alert('O boleto foi pago!');
+// }else if(boletoPago === false) {
+//     alert('O boleto não foi pago!');
+// }
---
+++ src/03-js/01-basico/23-exercicio-05/js/index.js
@@ -0,0 +1,6 @@
+for (let index = 0; index <= 10; index++) {
+    
+    let dobro = index * 2;
+
+    console.log("O dobro do número " + index + " é " + dobro);
+}
